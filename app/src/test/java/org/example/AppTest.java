7504edcaf2096edce24a39a68be739cc531f703c
m1m02630108 <m1m02630108@oit.ac.jp>
2026-07-08 11:38:05 +0900

    add AppTest project

--- app/src/test/java/org/example/AppTest.java
+++ app/src/test/java/org/example/AppTest.java
@@ -15,6 +15,5 @@ public class AppTest {
     @Test public void testAppReternHelloWorld() {
         App classUnderTest = new App();
         assertEquals("Hello World!", classUnderTest.getGreeting());
-    /* "Hello World!" -> "Hello World" に変更 */
     }
 }
