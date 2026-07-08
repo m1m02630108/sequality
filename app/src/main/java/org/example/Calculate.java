081085a6660e5a7c69f12b014e77ca0c9e4df373
m1m02630108 <m1m02630108@oit.ac.jp>
2026-07-08 12:39:37 +0900

    add calculate project

--- app/src/main/java/org/example/Calculate.java
+++ app/src/main/java/org/example/Calculate.java
@@ -5,4 +5,24 @@ public class Calculate {
     {
         return x + y;
     }
+
+    public int getIndividualFee(int age) {
+        if (age <= 12) {
+            return 1000;
+        } else {
+            return 2000;
+        }
+    }
+
+    public int getTotalFee(int age, int groupSize) {
+        int fee = getIndividualFee(age);
+        int total = fee * groupSize;
+
+        if (groupSize >= 10){
+            total = (int)(total * 0.9);
+        }
+
+        return total;
+    }
+
 }
--- app/src/test/java/org/example/CalculateTest.java
+++ app/src/test/java/org/example/CalculateTest.java
@@ -10,4 +10,20 @@ public class CalculateTest {
         int expected = 5;
         assertEquals(expected, calculate.sum(2, 3));
     }
+
+    @Test
+    public void testIndividualFee() {
+        Calculate calc = new Calculate();
+        assertEquals(1000, calc.getIndividualFee(10));
+        assertEquals(2000, calc.getIndividualFee(25));
+    }
+
+    @Test
+    public void testGetTotalFee() {
+        Calculate calc = new Calculate();
+        assertEquals(1000, calc.getTotalFee(10, 1));
+        assertEquals(2000, calc.getTotalFee(25, 1));
+        assertEquals(13500, calc.getTotalFee(10, 15));
+        assertEquals(27000, calc.getTotalFee(25, 15));
+    }
 }
